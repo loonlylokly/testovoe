80480b8e29d8816418d91467cadb6846041bd32e
loonlylokly <krivdinm@ya.ru>
2024-07-15 00:41:48 +0300

    Добавляет верстку модального окна

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,26 @@
 import "./styles/style.css";
+
+const authDialog = window.document.getElementById(
+  "authDialog",
+) as HTMLDialogElement | null;
+
+const openAuthDialogBtn = window.document.getElementById(
+  "openAuthDialogBtn",
+) as HTMLButtonElement | null;
+
+const closeAuthDialogBtn = window.document.getElementById(
+  "closeAuthDialogBtn",
+) as HTMLButtonElement | null;
+
+authDialog?.addEventListener("close", returnScroll);
+openAuthDialogBtn?.addEventListener("click", openAuthDialog);
+closeAuthDialogBtn?.addEventListener("click", () => authDialog?.close());
+
+function openAuthDialog() {
+  authDialog?.showModal();
+  document.body.classList.add("scroll-lock");
+}
+
+function returnScroll() {
+  document.body.classList.remove("scroll-lock");
+}
